2a8cec63e2b4e81201b50a60e30c24eb41cc2f82
Nicholas Ratti <nickratti@gmail.com>
2019-08-23 17:05:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,6 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?( board, index, player = "X")
   if board[index] == "X" || board[index] == "O"
     return true
-    # board[index] = "#{player}"
-  # elsif board[index] == " " || board[index] == "" || board[index] == nil
-    # return false
   else board[index] == " " || board[index] == "" || board[index] == nil
     return false
   end
