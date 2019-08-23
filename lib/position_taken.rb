71f31f1be91874a474ec65c2090b38bc0c7699d6
Nicholas Ratti <nickratti@gmail.com>
2019-08-23 16:57:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def position_taken( board, player_move, player = "X")
+def position_taken?( board, player_move, player = "X")
   index = player_move.to_i-1
   if board[index] == "X" || board[index] == "O"
     puts "That space is already taken.  Please try again"
