1b5ac9d910dbf50e9198a8cc416f620de5748195
Nicholas Ratti <nickratti@gmail.com>
2019-08-23 17:00:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?( board, index, player = "X")
   if board[index] == "X" || board[index] == "O" || board[index] == " " || board[index] == ""
     puts "That space is already taken.  Please try again"
+    return false
   else
     board[index] = "#{player}"
   end
